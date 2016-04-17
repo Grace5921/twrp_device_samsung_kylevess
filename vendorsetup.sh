ac6b49e5305576c9639d73787b2e58dd66971321
Sumit Rajdev <androidlover5842@gmail.com>
2016-04-17 22:45:53 +0530

    compile twrp by typing kylevess

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,15 @@
 for i in eng userdebug user; do
 	add_lunch_combo omni_kylevess-${i}
 done
+#Compile twrp by typing kylevess
+function kylevess()
+{
+    lunch omni_kylevess-userdebug
+    if [ $? -eq 0 ]; then
+        time mka -j10 recoveryimage
+    else
+        echo "No such item in brunch menu. Try 'breakfast'"
+        return 1
+    fi
+    return $?
+}
